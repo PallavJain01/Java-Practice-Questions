acd2e212283606ae52b8681bc5b9dbbb472da1b8
killer snake <123300427+PallavJain01@users.noreply.github.com>
2023-05-10 06:39:44 +0530

    cleaning up

--- src/main/projects/StrReverse.java
+++ src/main/projects/StrReverse.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 /*
  * Write a java program to prompt the user for input and then return the string but reversed
  * For example: if user enters "hello world" then output should be "dlrow olleh"
-*/
-
-
-public class StrReverse {
-  public void main() {
+ * 
+*/public class StrReverse {
+  public String main() {
     Scanner sc = new Scanner(System.in);
     String inp = sc.nextLine();
     sc.close();
@@ -17,6 +15,6 @@ public class StrReverse {
     for (int i = inp.length()- 1; i >= 0; i--) {
       out += inp.charAt(i);
     }
-    System.out.println(out);
+    return out;
   }
 }
