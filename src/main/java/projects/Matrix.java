323640bb6337af197af88845998375592d39fc03
killer snake <123300427+PallavJain01@users.noreply.github.com>
2023-06-15 10:53:55 +0530

    better docs

--- src/main/java/projects/Matrix.java
+++ src/main/java/projects/Matrix.java
@@ -2,15 +2,16 @@ package main.java.projects;
 
 class MatrixCalculator {
 
-  public String matrixToString(int[][] matrix) {
-    /*
-     * returns a stringified matrix (for integer matrix)
-     */
+  /**
+   * @param integerMatrix
+   * @return matrix in string format
+   */
+  public String matrixToString(int[][] integerMatrix) {
     String matrixString = "";
     matrixString += "[";
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; j++) {
-        matrixString += " " + matrix[i][j] + " ";
+    for (int i = 0; i < integerMatrix.length; i++) {
+      for (int j = 0; j < integerMatrix[i].length; j++) {
+        matrixString += " " + integerMatrix[i][j] + " ";
       }
       matrixString += "]\n[";
     }
@@ -18,6 +19,29 @@ class MatrixCalculator {
     return matrixString;
   }
 
+  /**
+   * @param floatMatrix
+   * @return matrix in string format
+   */
+  public String matrixToString(float[][] floatMatrix) {
+    String matrixString = "";
+    matrixString += "[";
+    for (int i = 0; i < floatMatrix.length; i++) {
+      for (int j = 0; j < floatMatrix[i].length; j++) {
+        matrixString += " " + floatMatrix[i][j] + " ";
+      }
+      matrixString += "]\n[";
+    }
+    matrixString = matrixString.substring(0, matrixString.length() - 2);
+    return matrixString;
+  }
+
+  /**
+   * 
+   * @param matrix_1
+   * @param matrix_2
+   * @return sum of matrices
+   */
   public int[][] add(int[][] matrix_1, int[][] matrix_2) {
     int[][] out = new int[matrix_1.length][matrix_1[0].length];
     for (int i = 0; i < matrix_1.length; i++) {
@@ -28,10 +52,13 @@ class MatrixCalculator {
     return out;
   }
 
+  /**
+   * 
+   * @param matrix_1
+   * @param matrix_2
+   * @return difference of matrices
+   */
   public int[][] subtract(int[][] matrix_1, int[][] matrix_2) {
-    /*
-     * returns the difference of the two matrices (i = j)
-     */
     int[][] out = new int[matrix_1.length][matrix_1[0].length];
     for (int i = 0; i < matrix_1.length; i++) {
       for (int j = 0; j < matrix_1[i].length; j++) {
@@ -41,59 +68,48 @@ class MatrixCalculator {
     return out;
   }
 
-  public int[][] transpose_2x2(int[][] matrix) {
-    /*
-     * returns the transpose of a 2x2 matrix
-     */
-    int temp = matrix[0][1];
-    matrix[0][1] = matrix[1][0];
-    matrix[1][0] = temp;
-    return matrix;
-  }
-
-  public int[][] transpose_3x3(int[][] matrix) {
-    /*
-     * returns the transpose of a 3x3 matrix
-     */;
-    int[][] out = matrix;
-    int temp = 0;
-    temp = out[0][1];
-    out[0][1] = out[1][0];
-    out[1][0] = temp;
-
-    temp = out[0][2];
-    out[0][2] = out[2][0];
-    out[2][0] = temp;
-
-    temp = out[1][2];
-    out[1][2] = out[2][1];
-    out[2][1] = temp;
+  /**
+   * 
+   * @param matrix
+   * @return transpose of matrix
+   */
+  public int[][] transpose(int[][] matrix) {
+    int[][] out = new int[matrix.length][matrix[0].length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        out[i][j] = matrix[j][i];
+      }
+    }
     return out;
-
   }
 
+  /**
+   * 
+   * @param matrix of order 2
+   * @return determinant of a 2x2 matrix
+   */
   public int determinant_2x2(int[][] matrix) {
-    /*
-     * returns the determinant value of a 2x2 matrix
-     */
     return ((matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]));
   }
 
+  /**
+   * @param matrix of order 3
+   * @return determinant of 3x3 matrix
+   */
   public int determinant_3x3(int[][] matrix) {
-    /*
-     * returns the determinant value of a 3x3 matrix
-     */
     return (matrix[0][0] * ((matrix[1][1] * matrix[2][2]) - (matrix[1][2] * matrix[2][1]))
-        - matrix[0][1] * ((matrix[1][0] * matrix[2][2]) - (matrix[1][2] * matrix[2][0]))
-        + matrix[0][2] * ((matrix[1][0] * matrix[2][1]) - (matrix[1][1] * matrix[2][0])));
-
+          - matrix[0][1] * ((matrix[1][0] * matrix[2][2]) - (matrix[1][2] * matrix[2][0]))
+          + matrix[0][2] * ((matrix[1][0] * matrix[2][1]) - (matrix[1][1] * matrix[2][0])));
   }
 
+  /**
+   * 
+   * @param matrix_1
+   * @param matrix_2
+   * @param order    of matrix
+   * @return product of matrices
+   */
   public int[][] multiply(int[][] matrix_1, int[][] matrix_2, int order) {
-    /*
-     * returns the product matrix of two matrices
-     * Currently only working for square matrices (i = j)
-     */
     int[][] product = new int[order][order];
     for (int i = 0; i < order; i++) {
       for (int j = 0; j < order; j++) {
@@ -105,18 +121,28 @@ class MatrixCalculator {
     return product;
   }
 
+  /**
+   * 
+   * @param matrix
+   * @param scalar to be multiplied
+   * @return multiplied matrix
+   */
   public int[][] multiplyScalar(int[][] matrix, int scalar) {
-    /*
-     * returns the matrix multiplied by a scalar(integer)
-     */
-    for (int i = 0; i++ < matrix.length;){
-      for(int j = 0; j++ < matrix[i].length;) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
         matrix[i][j] *= scalar;
       }
     }
     return matrix;
   }
 
+  /**
+   * 
+   * @param matrix
+   * @param row
+   * @param column
+   * @return minor matrix of specified rows and columns
+   */
   public static int[][] minor(int[][] matrix, int row, int column) {
     int[][] minor = new int[matrix.length][matrix[0].length];
     int Index_row = 0;
@@ -135,45 +161,50 @@ class MatrixCalculator {
     return minor;
   }
 
+  /**
+   *
+   * @param matrix
+   * @return cofactor matrix
+   */
   public int[][] cofactor(int[][] matrix) {
-    /*
-     * returns cofactor of a matrix
-     * only works for square matrices (i = j)
-     */
     int[][] cofactor = new int[matrix.length][matrix[0].length];
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
         cofactor[i][j] = determinant_2x2(minor(matrix, i, j));
       }
     }
-    cofactor[0][1] *= -1;
-    cofactor[1][0] *= -1;
-    cofactor[1][2] *= -1;
-    cofactor[2][1] *= -1;
+    for (int i = 0; i < cofactor.length; i++) {
+      for (int j = 0; j < cofactor[i].length; j++) {
+        if ((i + j) % 2 == 1) {
+          cofactor[i][j] *= -1;
+        }
+      }
+    }
     return cofactor;
   }
 
+  /**
+   * 
+   * @param matrix
+   * @return adjoint of matrix
+   */
   public int[][] adjoint(int[][] matrix) {
-    /*
-     * returns the adjoint of a matrix
-     */
-    return transpose_3x3(cofactor(matrix));
+    return transpose(cofactor(matrix));
   }
 
-  public String inverse(int[][] matrix) {
-    /*
-     * returns the inverse of a matrix
-     */
-
-    String inverse = "[";
-    for (int i = 0; i < cofactor(matrix).length; i++) {
-      for (int j = 0; j < cofactor(matrix)[i].length; j++) {
-        inverse += " " + cofactor(matrix)[i][j] + "/" + determinant_3x3(matrix) + " ";
+  /**
+   * 
+   * @param matrix
+   * @return Inverse of matrix
+   */
+  public float[][] inverse(int[][] matrix) {
+    float[][] inverse = new float[matrix.length][matrix[0].length];
+    int determinant = determinant_3x3(matrix);
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        inverse[i][j] = adjoint(matrix)[i][j] / determinant;
       }
-      inverse += "]\n[";
     }
-    inverse = inverse.substring(0, inverse.length() - 2);
-
     return inverse;
   }
 }
