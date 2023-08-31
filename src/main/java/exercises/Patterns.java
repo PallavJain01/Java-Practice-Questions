e3a22a7f241b83bebea674582affd5cc3fc9ab8a
killer snake <123300427+PallavJain01@users.noreply.github.com>
2023-08-31 22:09:13 +0530

    Update Patterns.java

--- src/main/java/exercises/Patterns.java
+++ src/main/java/exercises/Patterns.java
@@ -188,7 +188,18 @@ public class Patterns {
   }
 
   static void q17(int n) {
-    //TODO
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - i - 1; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j <= i; j++) {
+        System.out.print((char)('A' + j));
+      }
+      for (int j = i; j > 0; j--) {
+        System.out.print((char)('A' + j - 1));
+      }
+      System.out.println();
+    }
   }
 
   static void q18(int n) {
